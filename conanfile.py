5f394ed5933c31ce8a9e05e883c1d9c9f957b0c5
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:14 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostForeachConan(base.BoostBaseConan):
     name = "boost_foreach"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_foreach"
     lib_short_names = ["foreach"]
     header_only_libs = ["foreach"]
