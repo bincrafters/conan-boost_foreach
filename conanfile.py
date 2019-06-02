df6e8e4cee0af58d431ec26dd7344f097808debd
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,19 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostForeachConan(base.BoostBaseConan):
     name = "boost_foreach"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_foreach"
-    lib_short_names = ["foreach"]
-    header_only_libs = ["foreach"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_range",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
